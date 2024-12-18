6f288df093c4b84bc47e208919e06a5078da6064
nfloyd0221 <NFloyd0221@yahoo.com>
2024-12-17 22:47:58 -0500

    Update assist.py

--- assist.py
+++ assist.py
@@ -6,7 +6,7 @@ from dotenv import OPENAI_API_KEY
 #https://platform.openai.com/playground/assistants
 # Initialize the client and mixer
 load_dotenv()
-client = OpenAI(default_headers={"OpenAI-Beta": "assistants=v2"}, api_key = os.environ("OPENAI_API_KEY"))
+client = OpenAI(default_headers={"OpenAI-Beta": "assistants=v2"}, api_key = os.environ('OPENAI_API_KEY'))
 mixer.init()
 
 assistant_id = "asst_e1lRtkunXL6VOgsuiLRERBhK"
